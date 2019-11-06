1b3354267ab1cad739269e3927c5f80acb6f891c
Harold Buter <54801108+hbuter-rubrik@users.noreply.github.com>
2019-11-06 13:39:15 +0000

    Add files via upload

---
+++ show unprotected VMs.ps1
@@ -0,0 +1,20 @@
+﻿#install & Import Rubrik Module
+    Install-Module Rubrik
+    Import-Module Rubrik
+
+# set parameters
+$RubrikCluster="emea1-rbk01.rubrikdemo.com"
+$Credxmlpath = "C:\Users\harold.buter\Documents\rubrik.cred"
+$CredXML = Import-Clixml $Credxmlpath
+$OutPath = "C:\Users\harold.buter\Documents\output.csv"
+
+# connect to cluster
+Connect-Rubrik -Server $RubrikCluster -Credential $CredXML
+
+# show unprotected virtual machines
+$RubrikVMs=Get-RubrikVM
+$UnprotectedVMs=$RubrikVMs | Where-Object{$_.effectiveSlaDomainName -eq "Unprotected"}
+# write output into CSV file
+$UnprotectedVMs | Sort-Object Name | Select Name,ID | Export-Csv -Path "C:\Users\harold.buter\Documents\output.csv" -NoTypeInformation
+#Show ouput on screen
+$UnprotectedVMs | Sort-Object Name | Select Name,ID
