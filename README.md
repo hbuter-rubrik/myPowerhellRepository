138d8b298f8255a15fa47eb24ff60bf49a7ccd8c
Harold Buter <harold.buter@rubrik.com>
2019-11-07 07:57:13 +0000

    change authentication file to export and import to authenticate against Rubrik cluster

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 In this folder you will find snipits of powershell for Rubrik code I am using regulary
 
 Rubrik create credential file - 
-creates encrypted XML authentication file that can be used for authentication against rubrik cluster
+creates encrypted XML authentication file that can be used for authentication against rubrik cluster.
 
 Rubrik Authentication - 
 script to login into rubrik cluster using authentication file 
--- Rubrik Create credential file.ps1
+++ Rubrik Create credential file.ps1
@@ -3,11 +3,10 @@
 $RubrikCluster="emea1-rbk01.rubrikdemo.com"
 #$RubrikCreds=Get-Credential
 
-#$credential = $RubrikCreds
-#$credential | Export-Clixml -Path "C:\Users\harold.buter\Documents\rubrik.cred"
+$credential = $RubrikCreds
+$credential | Export-Clixml -Path "/Users/hbuter/rubrik.cred"
 
+# use new created credential file to authenticate and connect to cluster
 $Credxmlpath = "/Users/hbuter/rubrik.cred"
 $CredXML = Import-Clixml $Credxmlpath
-
-# connect to cluster
 Connect-Rubrik -Server $RubrikCluster -Credential $CredXML
